a81cfa84735eb8248a0b2e30a060d04bf8c8151e
wurui <wurui.wr@gmail.com>
2017-04-22 15:16:14 +0800

    sss

--- asset/index.js
+++ asset/index.js
@@ -1,6 +1,282 @@
-define([],function(){
-  return {
+define(['zepto','mustache'],function(undef,Mustache){
+
+    var tpl;
+    var queryString= function (query) {
+        var search = window.location.search + '';
+        if (search.charAt(0) != '?') {
+            return '';
+        }
+        else {
+            search = search.replace('?', '').split('&');
+            for (var i = 0; i < search.length; i++) {
+                if (search[i].split('=')[0] == query) {
+                    return decodeURI(search[i].split('=')[1]);
+                }
+            }
+            return '';
+        }
+    };
+    var bids = queryString('bids');
+
+    var OrderModel = {};
+    var Settings = {};
+
+    var syncCode = function (fn) {
+        var totalcount = 0;
+        var renderConf = {};
+        $('tr[data-id]').each(function (i, n) {
+            var $n = $(n),
+                bid = $n.attr('data-id'),
+                $number = $('.J_number', $n),
+                count = $number.val() * 1;
+            totalcount += count;
+            var setting = Settings[bid];
+            //console.log('Settings',Settings[bid])
+            renderConf[bid] = {
+                count: count,
+                tplcolor: $('.preview', $n).css('backgroundColor'),
+                text1: setting.text1,
+                text2: setting.text2,
+                carlogo: setting.carlogo ? 'http://www.shaomachetie.com/static/smct/img/carlogo/' + setting.carlogo + '.jpg' : null
+            }
+            //OrderModel.totalfee+=$number.val() * $number.attr('data-price');
+
+        });
+        //console.log(renderConf)
+
+        var cb_count = 0;
+        var gencodes = [];
+
+        var cb = function () {
+            cb_count++;
+            if (cb_count == totalcount) {
+                //console.log('OK');
+                fn(gencodes)
+            }
+
+        };
+
+        $.getJSON('http://www.shaomachetie.com/smct/gencode', {count: totalcount}, function (r) {
+            var codes = r.data;
+            gencodes = [].concat(codes);
+
+            for (var k in renderConf) {
+                var count = renderConf[k].count;
+                var i = 0;
+                while (i < count) {
+
+
+                    renderPaper(codes.shift(), renderConf[k], function (dataURL, code) {
+                        //console.log(code);
+                        //console.log(dataURL);
+
+                        $.post('http://www.shaomachetie.com/smct/uploadpaper', {
+                            code: code,
+                            base64: dataURL
+                        }, cb);
+                        //cb();
+                    });
+                    i++;
+                }
+            }
+
+
+        });
+
+
+    };
+
+    var syncView = function (trigger) {
+        switch (trigger) {
+            case 'amount':
+                var totalcount = 0;
+                OrderModel.totalfee = 0;
+
+                $('tr[data-id]').each(function (i, n) {
+                    var $n = $(n),
+                        $number = $('.J_number', $n);
+                    totalcount += $number.val() * 1;
+                    OrderModel.totalfee += $number.val() * $number.attr('data-price');
+
+                });
+                $('#totalcount').html(totalcount)
+
+
+                $('#totalfee').html(OrderModel.totalfee.toFixed(2));
+                if (OrderModel.deliveryfee) {
+                    OrderModel.totalsum = OrderModel.totalfee - -OrderModel.deliveryfee;
+                    $('#totalsum').html(OrderModel.totalsum.toFixed(2));
+                }
+
+
+                break
+            case 'address':
+                OrderModel.totalsum = OrderModel.totalfee - -OrderModel.deliveryfee;
+
+                $('#totalsum').html(OrderModel.totalsum.toFixed(2));
+                $('#btpay').attr('disabled', false)
+                break
+            case 'material':
+                break
+        }
+    };
+
+
+    var DeliveryAdmin = {
+        showDialog: function () {
+            $('#deliveryDialog').dialog({
+                title: '管理收货地址',
+                width: 600,
+                modal: true,
+                buttons: [
+                    {
+                        text: "确定",
+                        icons: {
+                            primary: "ui-icon-check"
+                        },
+                        click: function () {
+                            DeliveryAdmin.addAddress();
+                            $(this).dialog("close");
+                        }
+                    },
+                    {
+                        text: "取消",
+                        icons: {
+                            primary: "ui-icon-closethick"
+                        },
+                        click: function () {
+                            $(this).dialog("close");
+                        }
+                    }
+                ]
+            });
+        },
+        addAddress: function () {
+            var $f = $('#deliveryDialog>form'),
+                f = $f[0],
+                fields = 'name,phone,province,city,district,detail'.split(','),
+                addrObj = {},
+                addrToString = function () {
+                    if (/北京|天津|上海|重庆|/.test(this.province)) {
+                        this.city = '';
+                    }
+                    return [this.name, '(' + this.phone + ')', this.province, this.city, this.district, this.detail].join(' ')
+                };
+            for (var i = 0, field; field = fields[i++];) {
+                addrObj[field] = f[field].value;
+            }
+            //console.log(addrObj);
+            $('#delivery').html(addrToString.call(addrObj));
+            OrderModel.address = addrObj;
+            DeliveryAdmin.getDeliveryFee(addrObj, function (r) {
+                $('#deliveryfee').html(OrderModel.deliveryfee = (r - 0).toFixed(2));
+                syncView('address');
+            });
+
+
+        },
+        getDeliveryFee: function (addrObj, fn) {
+            $.getJSON('http://www.shaomachetie.com/smct/getdeliveryfee', addrObj, function (r) {
+                fn(r.data)
+            })
+        }
+    };
+
+
+    function render(r) {
+        if (r && r.data && r.data.length) {
+            var list = r.data;
+            var totalfee = 0;
+            for (var i = 0, n; n = list[i++];) {
+                totalfee += n.price;
+                Settings[n._id] = n.setting;
+            }debugger
+            $('#list').html(Mustache.render(tpl, {
+                data: list,
+                totalfee: totalfee.toFixed(2)
+            }));
+            OrderModel.totalfee = totalfee;
+
+           // $('[data-toggle="distpicker"]').distpicker({});
+        } else {
+            $('#list').html('<i class="iconfont">&#xe631;</i>&nbsp;&nbsp;<br/>购物车是空的,赶紧去定制一个你喜欢的车贴吧~<br/><a href="builder.html">开始定制</a>').addClass('empty-order');
+        }
+    };
+
+    return {
     init:function($mod){
+        tpl=$('.J_tpl',$mod).html();
+        var $list=$('.J_list',$mod);
+        $.getJSON('http://www.shaomachetie.com/smct/getbuilds?bids='+bids+'&callback=?', function render(r) {
+            if (r && r.data && r.data.length) {
+                var list = r.data;
+                var totalfee = 0;
+                for (var i = 0, n; n = list[i++];) {
+                    totalfee += n.price;
+                    Settings[n._id] = n.setting;
+                }
+                $list.html(Mustache.render(tpl, {
+                    data: list,
+                    totalfee: totalfee.toFixed(2)
+                }));
+                OrderModel.totalfee = totalfee;
+
+                // $('[data-toggle="distpicker"]').distpicker({});
+            } else {
+                $list.html('<i class="iconfont">&#xe631;</i>&nbsp;&nbsp;<br/>购物车是空的,赶紧去定制一个你喜欢的车贴吧~<br/><a href="builder.html">开始定制</a>').addClass('empty-order');
+            }
+        });
+
+        $list.on('change', function (e) {
+            var $tar = $(e.target);
+            switch (true) {
+                case $tar.hasClass('J_number'):
+                    syncView('amount');
+                    break
+            }
+        })
+
+        //var f = document.getElementById('orderform');
+        var loading = false,
+            btpay = $('#btpay').on('click', function () {
+                if (loading) {
+                    btpay.addClass('loading')
+                    return false
+                }
+                loading=true;
+                var pack = [];
+                $('tr[data-id]').each(function (i, n) {
+                    var $n = $(n);
+                    pack.push({
+                        bid: $n.attr('data-id'),
+                        amount: $('.J_number', $n).val(),
+                        material: $('.J_material', $n).val()
+                    })
+                });
+                syncCode(function (codes) {
+                    $.post('/smct/submitorder', {
+                        pack: JSON.stringify(pack),
+                        delivery: JSON.stringify(OrderModel.address),
+                        codes: JSON.stringify(codes),
+                        totalcount: $('#totalcount').html() - 0,
+                        totalfee: $('#totalfee').html() - 0,
+                        deliveryfee: OrderModel.deliveryfee,
+                        totalsum: OrderModel.totalsum
+                    }, function (r) {
+                        loading=false;
+                        btpay.removeClass('loading')
+                        if (r.error) {
+                            alert(r.error)
+                        } else {
+                            // alert('支付去吧~~')
+                            location.href = '/alipay/pay?from=smct&oid=' + r.data;
+                        }
+
+                    });
+                });
+
+                return false;
+            });
 
     }
   }
--- mod.xsl
+++ mod.xsl
@@ -2,9 +2,97 @@
     <xsl:template match="/root" name="wurui.smct-shopcart">
         <!-- className 'J_OXMod' required  -->
         <div class="J_OXMod oxmod-smct-shopcart" ox-mod="smct-shopcart">
-            <h1>
-                This is mod smct-shopcart;
-            </h1>
+            <div class="J_list list"></div>
+
+            <div class="lrbar">
+                <span>
+                    收货地址:<span class="J_address"></span>
+                </span>
+            </div>
+            <div class="lrbar">
+                <span>发货城市:杭州</span>
+                <span>
+                    运费: <span class="J_delieryfee price">--.--</span>
+                </span>
+            </div>
+            <hr/>
+            <div class="lrbar">
+                <span>支付方式:
+                    <select>
+                        <option>支付宝</option>
+                    </select>
+                </span>
+                <span>
+                    实付金额: <big class="J_totalfee price">--.--</big>
+                </span>
+            </div>
+            <div class="lrbar">
+                <span></span>
+                <span>
+                    <button class="bt-order">提交订单</button>
+                </span>
+            </div>
+
+
+        <script type="text/tpl" class="J_tpl"><![CDATA[
+            <table class="order-table" cellpadding="0" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>预览</th>
+                        <th>数量</th>
+                        <th>价格</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {{#data}}
+                    <tr data-id="{{_id}}">
+                        <td width=160>
+                        <div class="snapshot">
+                            <div class="preview bgcolor-{{setting.bgcolor}}"">
+                            <div class="card-header">{{setting.text1}}</div>
+                            <div class="card-body tpl tpl-{{setting.tpl}}">
+                                <div class="central">
+                                    {{#setting.carlogo}}<img src="http://www.shaomachetie.com/static/smct/img/carlogo/{{setting.carlogo}}.jpg"/>{{/setting.carlogo}}
+                                </div>
+                                <img src="http://i.oxm1.cc/uploads/git/wurui/img/2ahkwkkveTj1rgh0ueRlcquA5vz-1000.png" class="qrcode"/>
+
+                            </div>
+                            <div class="card-footer">
+                                <span>{{setting.text2}}</span>
+                            </div>
+                        </div>
+
+                    </div>
+                </td>
+                <td>
+                    <button type="button" class="amount-up"></button>
+                    <br/>
+                    <input type="number" data-price="{{price}}" class="J_number amount-input" value="1" min="0" size="1"/>
+                    <br/>
+                    <button type="button" class="amount-down"></button>
+                </td>
+                <td>
+                    <span class="price">{{price}}</span>
+                </td>
+            </tr>
+            {{/data}}
+
+
+        </tbody>
+
+    </table>
+
+    <hr/>
+    <div class="lrbar">
+        <span>
+            商品总计: <span id="totalcount">{{data.length}}</span>张
+        </span>
+        <span>
+            商品总价: <span id="totalfee" class="price">{{totalfee}}</span>
+        </span>
+    </div>
+]]>
+</script>
         </div>
     </xsl:template>
 </xsl:stylesheet>
